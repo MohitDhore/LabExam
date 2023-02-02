7d677fb2465dd548b67a6323a1f2e9fe938ae54a
Mohit Dhore <dhoremohit@gmail.com>
2023-02-02 15:32:43 +0530

    two commit

--- LinkedList.java
+++ LinkedList.java
@@ -4,20 +4,37 @@ class LinkedList {
     static class Node {
         int data;
         Node next;
+       
 
         Node(int data) {
             this.data = data;
             next = null;
 
         }
+    }
 
-        public static void main(String[] args) {
-            LinkedList lst = new LinkedList();
-            head = new Node(10);
-            Node Second = new Node(20);
-            Node Third = new Node(30);
-            Node Fourth = new Node(40);
-
+    static void insertion_at_end(int data) {
+        Node new_node = new Node(data);
+        if (head == null) {
+            head = new Node(data);
+        }
         
+        new_node.next = null;
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+            last.next = new_node;
+            return;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList lst = new LinkedList();
+        head = new Node(10);
+        Node Second = new Node(20);
+        Node Third = new Node(30);
+        Node Fourth = new Node(40);
+
     }
 }
